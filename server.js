3970a3df1d07e0985c698579a634ded980898901
Ayan Manna <ayanmanna1234@gmail.com>
2025-04-26 02:49:47 +0530

    route imported to server

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoute"));
+app.use("/api/users", require("./routes/userRoute"));
 app.use(errorHandler);
 
 app.listen(port, () => {
