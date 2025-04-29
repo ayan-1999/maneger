25a0966c6b37b99e23c35240d0d1c65209b1fe6a
Ayan Manna <ayanmanna1234@gmail.com>
2025-04-29 15:35:54 +0530

    configuration: cors and jwt added.

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
+const cors = require("cors")
 
 connectDb();
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors())
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoute"));
